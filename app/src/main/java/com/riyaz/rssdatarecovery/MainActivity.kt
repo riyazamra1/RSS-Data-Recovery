210f6984bf9a065392d97c916a7ce4bcb57c8acf
Mohamed Riyaz <86571297+riyazamra1@users.noreply.github.com>
2026-09-17 12:02:26 +0530

    Switch app entry point to functional recovery feature build

--- app/src/main/java/com/riyaz/rssdatarecovery/MainActivity.kt
+++ app/src/main/java/com/riyaz/rssdatarecovery/MainActivity.kt
@@ -9,7 +9,7 @@ import androidx.activity.compose.setContent
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { RecoveryAppV2() }
+        setContent { RecoveryAppV3() }
     }
 }
 
