8e0d56ae0e92ededac6c0c63a41f98fe6f40acac
Mohamed Riyaz <86571297+riyazamra1@users.noreply.github.com>
2026-09-17 07:42:28 +0530

    Use revised RSS Data Recovery UI

--- app/src/main/java/com/riyaz/rssdatarecovery/MainActivity.kt
+++ app/src/main/java/com/riyaz/rssdatarecovery/MainActivity.kt
@@ -9,7 +9,7 @@ import androidx.activity.compose.setContent
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { RecoveryApp() }
+        setContent { RecoveryAppV2() }
     }
 }
 
